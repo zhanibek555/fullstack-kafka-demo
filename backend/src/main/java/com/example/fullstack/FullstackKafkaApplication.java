764bc383fc89e3959ec339413f543cde0ca66152
zhanibek555 <47441428+zhanibek555@users.noreply.github.com>
2025-08-26 21:25:04 -0700

    Create FullstackKafkaApplication.java

---
+++ backend/src/main/java/com/example/fullstack/FullstackKafkaApplication.java
@@ -0,0 +1,11 @@
+package com.example.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackKafkaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackKafkaApplication.class, args);
+    }
+}
